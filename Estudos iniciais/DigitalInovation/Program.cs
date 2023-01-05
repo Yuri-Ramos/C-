15ecd9032dbbdfb3815a86dbd99b9cc75eaafd5b
Yuri Ramos <yroramos@gmail.com>
2023-01-05 17:25:09 -0300

    :zap: continuação da aula;

--- Estudos iniciais/DigitalInovation/Program.cs
+++ Estudos iniciais/DigitalInovation/Program.cs
@@ -1,2 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+int numeroDeVezes = 100;
+for (int i = 0; i<numeroDeVezes; i++){
+
+Console.WriteLine($"Bem vindo ao numero de vezes {i}");
+}
