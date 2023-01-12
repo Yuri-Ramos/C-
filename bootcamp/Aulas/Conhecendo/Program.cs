72aa4e60e9fe23952ca97c06517b90dbaf3e743d
Yuri Ramos <yroramos@gmail.com>
2023-01-12 15:46:16 -0300

    namespace

--- bootcamp/Aulas/Conhecendo/Program.cs
+++ bootcamp/Aulas/Conhecendo/Program.cs
@@ -1,4 +1,2 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine("hello world!");
-
+﻿using Conhecendo.Models;
+Pessoa p = new Pessoa();
