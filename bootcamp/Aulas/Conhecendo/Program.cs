bc6be6b32a2295ed6bcc8c3fef3ac277536fa39c
Yuri Ramos <yroramos@gmail.com>
2023-01-16 10:04:12 -0300

    Syntaxes :zap:

--- bootcamp/Aulas/Conhecendo/Program.cs
+++ bootcamp/Aulas/Conhecendo/Program.cs
@@ -1,20 +1,24 @@
 ﻿using Conhecendo.Models;
 
-string apresentacao = "olá seja bem vindo";
-int quantidade = 1;
-double altura = 1.80;
-
-decimal preco = 1.80M;
-
-bool condicao = true;
+DateTime dataAtual = DateTime.Now.AddDays(5);
+Console.WriteLine(dataAtual.ToString("yyyy-MM-dd"));
 
 
-Console.WriteLine($"{apresentacao},{quantidade},{altura.ToString("0-00")},{preco},{condicao}");
+// string apresentacao = "olá seja bem vindo";
+// int quantidade = 1;
+// double altura = 1.80;
 
-// instancia de objeto com canelcase
-Pessoa pessoa1 = new Pessoa();
+// decimal preco = 1.80M;
 
-pessoa1.Nome ="Yuri";
-pessoa1.Idade = 18;
+// bool condicao = true;
 
-pessoa1.Apresentar();
+
+// Console.WriteLine($"{apresentacao},{quantidade},{altura.ToString("0-00")},{preco},{condicao}");
+
+// // instancia de objeto com canelcase
+// Pessoa pessoa1 = new Pessoa();
+
+// pessoa1.Nome ="Yuri";
+// pessoa1.Idade = 18;
+
+// pessoa1.Apresentar();
