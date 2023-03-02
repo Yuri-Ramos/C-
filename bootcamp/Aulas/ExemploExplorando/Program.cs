df27266b46ced2b8b6b76e09ca4958503d3e69eb
Yuri Ramos <yroramos@gmail.com>
2023-03-02 16:31:31 -0300

    Propriedades

--- bootcamp/Aulas/ExemploExplorando/Program.cs
+++ bootcamp/Aulas/ExemploExplorando/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using ExemploExplorando.models;
+
+Pessoa p1 = new Pessoa();
+Pessoa p2 = new Pessoa();
+
+p1.Nome = "Yuri";
+p1.Idade = 22;
+p2.Nome = "Joaquina";
+p2.Idade = 2;
+p1.Sobrenome="ferreira";
+p2.Sobrenome="Pagodeiro";
+
+p1.Apresentar();
+p2.Apresentar();
---
+++ bootcamp/Aulas/ExemploExplorando/models/Pessoa.cs
@@ -0,0 +1,52 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExemploExplorando.models
+{
+    public class Pessoa
+    {
+        
+        private string _nome; // não aceita valores nulos
+        public string Nome
+        {
+            get => _nome.ToUpper();
+
+            set
+            {
+
+
+                if (value == "")
+                {
+                    throw new ArgumentException("o nome nao pode ser vazio");
+                }
+                _nome = value;
+            }
+
+
+        }
+        // get e setter são para propriedades
+        public string Sobrenome { get; set; }
+        public string NomeCompleto => $"{Nome} {Sobrenome}".ToUpper();	
+    
+
+    private int _idade; 
+        public int Idade { get=> _idade;
+
+            set {
+            if (value<0)
+            {
+                throw new ArgumentException("idade nao pode ser Menor que zero.");
+            }
+            _idade = value;
+        }}
+
+        public void Apresentar()
+        {
+            Console.WriteLine($"Nome: {Nome}, Idade: {Idade}, {NomeCompleto}");
+        }
+
+
+    }
+}
