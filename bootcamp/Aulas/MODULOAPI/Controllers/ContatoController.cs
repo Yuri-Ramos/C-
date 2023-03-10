64e400a2ae9a57646472eb5ff0b1dbde76f849c1
Yuri Ramos <yroramos@gmail.com>
2023-03-10 15:35:33 -0300

    Crud completo

--- bootcamp/Aulas/MODULOAPI/Controllers/ContatoController.cs
+++ bootcamp/Aulas/MODULOAPI/Controllers/ContatoController.cs
@@ -30,11 +30,55 @@ namespace MODULOAPI.Controllers
         public IActionResult ObterId(int id)
         {
             var contato = _context.Contatos.Find(id);
+            //verifica se contato é null
             if (contato == null)
             {
                 return NotFound();
             }
             return Ok(contato);
         }
+
+        [HttpPut("{id}")]
+        public IActionResult Edit(int id, Contato contato)
+        {
+                var contatoBanco = _context.Contatos.Find(id);
+                  if (contatoBanco == null)
+            {
+                return NotFound();
+            }
+            contatoBanco.Nome = contato.Nome;
+            contatoBanco.Telefone = contato.Telefone;
+            contatoBanco.Ativo = contato.Ativo;
+            _context.Contatos.Update(contatoBanco);
+            _context.SaveChanges();
+            return Ok(contatoBanco);
+                
+        }
+
+    public IActionResult Deletar(int id)
+    {
+        var contatoBanco = _context.Contatos.Find(id);
+        if (contatoBanco == null)
+        {
+            return NotFound();
+        }
+        _context.Contatos.Remove(contatoBanco);
+        _context.SaveChanges();
+        var message = $"contato : {contatoBanco.Nome} removido com sucesso.";
+        return Ok(message);
+            //return NoContent();
+        }
+        [HttpGet("{nome}")]
+        public IActionResult ObterPorNome(string nome)
+        {
+            var contatoBanco = _context.Contatos.Where(x => x.Nome.Contains(nome));
+            if (contatoBanco == null)
+            {
+                return NotFound();
+            }
+            return Ok(contatoBanco);
+
+        }
+
     }
 }
